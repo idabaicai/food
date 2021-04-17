086a1003abb9a56e5c1c5c5344bacb4a9d5b034b
dabai <icandebug@gmail.com>
2021-04-17 10:02:31 +0800

    引入element插件

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
-    app
+    <el-button>app</el-button>
   </div>
 </template>
-
+<script>
+export default {
+  created () {
+    this.$message.success('se')
+  }
+}
+</script>
 <style lang="less">
 </style>
