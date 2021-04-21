270f074ba115b333baa13ae2f845bc27c99d16d1
idabaicai <icandebug@gmail.com>
2021-04-21 10:49:25 +0800

    购物车结算计算器组件

---
+++ src/components/calculator/Calculator.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="calc">
+    <el-input class="input" v-model="number">
+       <el-button slot="prepend" icon="el-icon-minus" @click="handleMinux" :disabled="this.number===1"></el-button>
+       <el-button slot="append" icon="el-icon-plus" @click="handlePlus"></el-button>
+    </el-input>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Calculator',
+  props: {
+    num: {
+      type: Number,
+      default () {
+        return 1
+      }
+    }
+  },
+  data () {
+    return {
+      number: 1
+    }
+  },
+  methods: {
+    handleMinux () {
+      if (this.num > 1) {
+        this.number--
+        this.$emit('numChange', this.number)
+      }
+    },
+    handlePlus () {
+      this.number++
+      this.$emit('numChange', this.number)
+    }
+  },
+  created () {
+    this.number = this.num // Avoid mutating a prop directly
+  }
+}
+</script>
+<style lang="less" scoped>
+  .calc {
+    .el-input {
+      width: 170px;
+      /deep/ input {
+        text-align: center;
+      }
+    }
+  }
+</style>
--- src/views/user/components/Collect.vue
+++ src/views/user/components/Collect.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="container">
     <div class="item">
-      item
+      <el-row v-for="item in itemList" :key="item.id">
+        <el-col :span="3">
+          <img :src="item.img_path" :alt="item.name">
+        </el-col>
+        <el-col :span="5">
+          <div class="title"> {{ item.name }} </div>
+          <el-rate v-model="item.star" disabled show-score text-color="#f90"></el-rate>
+        </el-col>
+        <el-col :span="4" :offset="2">
+          ￥{{ item.price }}
+        </el-col>
+        <el-col :span="6">
+          <calculator :num="item.number" @numChange="handleNumChange"></calculator>
+        </el-col>
+      </el-row>
     </div>
   </div>
 </template>
 <script>
+import Calculator from '../../../components/calculator/Calculator'
 export default {
   name: 'Collect',
+  components: {
+    Calculator
+  },
   data () {
     return {
       itemList: [
-        { id: 1, name: '孜然鸡柳+百事可乐', img_path: require('../../../assets/list/item1.jpg') }
+        { id: 1, name: '孜然鸡柳+百事可乐', star: 4.7, img_path: require('../../../assets/list/item1.jpg'), price: 29.9, number: 1 },
+        { id: 2, name: '美滋客汉堡', star: 4.8, img_path: require('../../../assets/list/item2.jpg'), price: 39.9, number: 3 }
       ]
+    }
+  },
+  methods: {
+    handleNumChange (num) {
+      console.log(num)
     }
   }
 }
@@ -20,5 +44,21 @@ export default {
 <style lang="less" scoped>
   .container {
     padding: 15px;
+    .item {
+      width: 100%;
+      .el-row {
+        padding: 20px 0 !important;
+        border-bottom: 1px solid #ddd;
+        margin-bottom: 10px;
+      }
+      img {
+        width: 80px;
+        height: 80px;
+        border-radius: 4px;
+      }
+      .title {
+        color: #303133;
+      }
+    }
   }
 </style>
