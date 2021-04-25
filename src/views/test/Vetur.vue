bbdf999b9f40ac14a194794ade1e00781019ddaf
idabaicai <icandebug@gmail.com>
2021-04-25 09:24:43 +0800

    引入prettier

---
+++ src/views/test/Vetur.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>快捷键： vue</p>
+    <button></button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    say () {
+      // console.log("hello");
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
