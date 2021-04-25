4fb3c8b3c5063fa7e3e8736347b4129ce3917655
idabaicai <icandebug@gmail.com>
2021-04-25 18:09:41 +0800

    feat: 注册接口

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,9 +1,22 @@
 import axios from 'axios'
 
-const baseURL = 'https://www.fastmock.site/mock/3f5566f50a1f5ad65570d3d161b2f5f5/food'
-const post = (url, data) => {
+const baseURL = 'http://114.55.140.167:8091'
+// const baseURL = 'https://www.fastmock.site/mock/3f5566f50a1f5ad65570d3d161b2f5f5/food'
+const post = (url, params) => {
   return new Promise((resolve, reject) => {
-    axios.post(url, data, {
+    axios.post(url, params, {
+      baseURL,
+      'Content-type': 'application/json'
+    }).then(res => {
+      resolve(res)
+    }).catch(error => {
+      reject(error)
+    })
+  })
+}
+const get = (url) => {
+  return new Promise((resolve, reject) => {
+    axios.get(url, {
       baseURL,
       'Content-type': 'application/json'
     }).then(res => {
@@ -14,5 +27,6 @@ const post = (url, data) => {
   })
 }
 export default {
-  post
+  post,
+  get
 }
