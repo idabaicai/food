0154753bee8a2f3f39c531b8008589137265bda7
bz-dj <jiang.ding@biz-united.com.cn>
2021-04-28 10:24:20 +0800

    轮播图

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,39 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
     'plugin:vue/essential',
-    '@vue/standard'
+    // '@vue/airbnb',
   ],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'Missing space before function parentheses': 'off'
-  }
-}
+    "max-len" : ["error", {code : 300}],
+    "no-restricted-syntax": 0,
+    "class-methods-use-this": 0,
+    "no-underscore-dangle":  'off',
+    "no-return-assign":0,
+    "consistent-return": 0,
+    "no-unused-vars": 0,
+    "global-require": 0,
+    "no-undef": 0,
+    "import/extensions": 0,
+    "import/no-unresolved": 0
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        mocha: true,
+      },
+    },
+  ],
+};
--- src/views/home/FoodMain.vue
+++ src/views/home/FoodMain.vue
@@ -6,11 +6,10 @@
           <h4>全部分类</h4>
         </div>
         <ul>
-          <li
-            v-for="item in cateLists"
-            :key="item.id"
-          >
-            <a href="#" @click.prevent="handleCateClick(item.id)"> {{ item.name }}</a>
+          <li v-for="item in cateLists" :key="item.id">
+            <a href="#" @click.prevent="handleCateClick(item.id)">
+              {{ item.name }}</a
+            >
             <i class="el-icon-arrow-right"></i>
           </li>
         </ul>
@@ -24,8 +23,9 @@
                 class="swiper-slide"
                 v-for="item in swipeList"
                 :key="item.id"
+                @click="toDetail(item.foodId)"
               >
-              <img :src="item.img_path" alt="">
+                <img :src="item.img_path" alt="" />
               </div>
             </div>
           </div>
@@ -39,7 +39,7 @@
               v-for="item in subSwipeList"
               :key="item.id"
             >
-              <img :src="item.img_path" alt="">
+              <img :src="item.img_path" alt="" />
             </el-col>
           </div>
         </div>
@@ -76,7 +76,7 @@
           </div>
         </div>
         <div class="right-bottom">
-          <img src="../../assets/mini-app.png" alt="">
+          <img src="../../assets/mini-app.png" alt="" />
           <div class="qr-code">
             <span class="info-text">扫一扫, 马上订</span>
           </div>
@@ -92,8 +92,7 @@ import 'swiper/swiper-bundle.css'
 import request from '../../utils/request'
 export default {
   name: 'FoodMain',
-  components: {
-  },
+  components: {},
   data () {
     return {
       // 右侧分类数据
@@ -102,9 +101,21 @@ export default {
       ],
       // 轮播图
       swipeList: [
-        { id: 101, img_path: require('../../assets/slides/item1.png') },
-        { id: 102, img_path: require('../../assets/slides/item2.jpg') },
-        { id: 103, img_path: require('../../assets/slides/item3.jpg') }
+        {
+          id: 101,
+          img_path: require('../../assets/slides/item1.png'),
+          foodId: 1
+        },
+        {
+          id: 102,
+          img_path: require('../../assets/slides/item2.jpg'),
+          foodId: 2
+        },
+        {
+          id: 103,
+          img_path: require('../../assets/slides/item3.jpg'),
+          foodId: 3
+        }
       ],
       // 二级轮播
       subSwipeList: [
@@ -132,6 +143,10 @@ export default {
     handleMyOrder () {
       this.$router.push({ path: `user/${this.userId}/Order` })
     },
+    // 去商品详情
+    toDetail (id) {
+      this.$router.push({ path: `/detail/${id}` })
+    },
     // 去收藏页面
     handleCollect () {
       this.$router.push({ path: `user/${this.userId}/Collect` })
@@ -141,18 +156,15 @@ export default {
       this.$router.push({ path: `list/${cateId}` })
     }
   },
-  computed: {
-  },
-  watch: {
-  },
+  computed: {},
+  watch: {},
   created () {
     // 获取分类数据
 
-    request.post('/item/findItemList')
-      .then(res => {
-        console.log(res)
-        this.cateLists = res.data.data
-      })
+    request.post('/item/findItemList').then((res) => {
+      console.log(res)
+      this.cateLists = res.data.data
+    })
     // 要在next中执行
     this.$nextTick(() => {
       this.initSwiper()
@@ -160,142 +172,142 @@ export default {
     // 缓存当前用户id
     this.userId = localStorage.getItem('uid')
   },
-  mounted () {
-  }
+  mounted () {}
 }
 </script>
 <style lang="less" scoped>
 @import url('../../style/variables.less');
-  .main {
+.main {
+  height: 427px;
+  margin-bottom: 30px;
+  .wrap {
     height: 427px;
-    margin-bottom: 30px;
-    .wrap {
-      height: 427px;
+  }
+  .left {
+    margin-top: -50px;
+    height: 477px;
+    background-color: #fff;
+    border: 1px solid #ddd;
+    .title {
+      padding: 15px 12px;
+      margin-bottom: 12px;
+      color: @primary-text-color;
     }
-    .left {
-      margin-top: -50px;
-      height: 477px;
-      background-color: #fff;
-      border: 1px solid #ddd;
-      .title {
-        padding: 15px 12px;
-        margin-bottom: 12px;
-        color: @primary-text-color;
+    li {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 100%;
+      height: 32px;
+      padding: 0 12px;
+      &:hover {
+        background-color: rgb(255, 245, 235);
       }
-      li {
+      a,
+      i {
+        color: #c0c4cc;
+        font-size: 12px;
+      }
+    }
+  }
+  .center {
+    // background-color: purple;
+    width: 690px;
+    margin: 0 5px;
+    .center-top {
+      width: 100%;
+      height: 290px;
+      margin-bottom: 10px;
+      // background-color: #0d0;
+      .swiper-container {
         display: flex;
-        justify-content: space-between;
-        align-items: center;
         width: 100%;
-        height: 32px;
-        padding: 0 12px;
-        &:hover {
-          background-color: rgb(255, 245, 235);
-        }
-        a, i {
-          color: #C0C4CC;
-          font-size: 12px;
-        }
+        height: 100%;
       }
-    }
-    .center {
-      // background-color: purple;
-      width: 690px;
-      margin: 0 5px;
-      .center-top {
-        width: 100%;
-        height: 290px;
-        margin-bottom: 10px;
-        // background-color: #0d0;
-        .swiper-container {
-          display: flex;
+      .swiper-slide {
+        font-size: 5rem;
+        text-align: center;
+        img {
           width: 100%;
           height: 100%;
-        }
-        .swiper-slide {
-          font-size: 5rem;
-          text-align: center;
-          img {
-            width: 100%;
-            height: 100%;
-            cursor: pointer;
-          }
-        }
-      }
-      .center-bottom {
-        width: 690px;
-        height: 127px;
-        .el-row {
-          // width: 690px;
-          margin: 0;
-          padding: 0;
-        }
-        .item {
-          width: 226px;
-          height: 127px;
-          &:nth-child(2) {
-            margin: 0 6px;
-          }
-          img {
-            width: 100%;
-            height: 100%;
-          }
+          cursor: pointer;
         }
       }
     }
-    .right {
-      .right-top {
-        width: 100%;
-        height: 290px;
-        margin-bottom: 10px;
-        padding: 0 20px;
-        border: 1px solid #ddd;
-        background-color: #fff;
-        .avatar {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          height: 120px;
-        }
-        .mine-box {
-          .el-row {
-              display: flex;
-              justify-content: center;
-              flex-wrap: wrap;
-              height: 50px;
-            .el-col {
-              display: flex;
-              flex-direction: column;
-              justify-content: space-between;
-              align-items: center;
-              width: 60px;
-              height: 46px;
-              margin: 8px 5px;
-              cursor: pointer;
-              .iconfont {
-                font-size: 40px;
-                color: rgb(240, 150, 40);
-              }
-              .item {
-                display: block;
-              }
-            }
-          }
-        }
+    .center-bottom {
+      width: 690px;
+      height: 127px;
+      .el-row {
+        // width: 690px;
+        margin: 0;
+        padding: 0;
       }
-      .right-bottom {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
+      .item {
+        width: 226px;
         height: 127px;
-        background-color: #fff;
-        border: 1px solid #ddd;
+        &:nth-child(2) {
+          margin: 0 6px;
+        }
         img {
-          width: 100px;
-          height: 100px;
+          width: 100%;
+          height: 100%;
         }
       }
     }
   }
+  .right {
+    .right-top {
+      width: 100%;
+      height: 290px;
+      margin-bottom: 10px;
+      padding: 0 20px;
+      border: 1px solid #ddd;
+      background-color: #fff;
+      .avatar {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 120px;
+      }
+      .mine-box {
+        .el-row {
+          display: flex;
+          justify-content: center;
+          flex-wrap: wrap;
+          height: 50px;
+          .el-col {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            align-items: center;
+            width: 60px;
+            height: 46px;
+            margin: 8px 5px;
+            cursor: pointer;
+            .iconfont {
+              font-size: 40px;
+              color: rgb(240, 150, 40);
+            }
+            .item {
+              display: block;
+            }
+          }
+        }
+      }
+    }
+    .right-bottom {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      height: 127px;
+      background-color: #fff;
+      border: 1px solid #ddd;
+      img {
+        width: 100px;
+        height: 100px;
+      }
+    }
+  }
+}
 </style>
